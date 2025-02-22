271ce2683e295e214851809f8416d81ca411953b
hirotaka06 <h.kojima572@gmail.com>
2025-02-22 17:02:51 +0900

    Configure Prettier and ESLint for shadcn/ui

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,10 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['node_modules', 'dist', 'app/components/ui'],
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
