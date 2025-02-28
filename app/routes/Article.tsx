90dd0ed12053c575502df6c4df4240747e45a2bc
hirotaka06 <h.kojima572@gmail.com>
2025-03-01 00:06:25 +0900

    Enhance Article page

--- app/routes/Article.tsx
+++ app/routes/Article.tsx
@@ -1,8 +1,11 @@
 import type { Route } from './+types/Article';
 import { Link } from 'react-router';
+import { Button } from '~/components/ui/button';
+import { useFetchArticle } from '~/hooks/useFetchArticle';
+import { useEffect } from 'react';
+import { ArrowLeft } from 'lucide-react';
 import { useAtomValue } from 'jotai';
 import { apiTokenAtom } from '~/atoms/articleAtoms';
-import { useFetchArticle } from '~/hooks/useFetchArticle';
 
 export function meta({ params }: Route.MetaArgs) {
   const { articleId } = params;
@@ -19,15 +22,32 @@ export default function Article({ params }: Route.ComponentProps) {
     apiToken,
   );
 
+  useEffect(() => {
+    const articleElement = document.querySelector('.article-content');
+    if (articleElement) {
+      const h1Elements = articleElement.getElementsByTagName('h1');
+      for (const h1 of h1Elements) {
+        h1.classList.add('font-bold', 'text-2xl');
+      }
+    }
+  }, [data?.rendered_body]);
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="h-screen flex items-center justify-center text-white">
-      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-3xl">
-        <h1 className="text-2xl font-bold">{data?.title}</h1>
+    <div className="flex flex-col items-center justify-center mt-4">
+      <div className="p-6 w-full md:w-[calc(100%-4rem)] lg:w-[calc(100%-8rem)] text-white gap-4">
+        <Link to="/">
+          <Button className="text-black bg-white hover:bg-gray-200">
+            <ArrowLeft />
+          </Button>
+        </Link>
+        <div
+          className="article-content mt-2"
+          dangerouslySetInnerHTML={{ __html: data?.rendered_body ?? '' }}
+        />
       </div>
-      <Link to="/">戻る</Link>
     </div>
   );
 }
